caf0b0bb0e9445a68b33518721ef92566935d602
Leon Mergen <leon@solatis.com>
2025-04-09 10:55:19 +0700

    Harden script against executing from any directory

--- scripts/teamcity/20.test.sh
+++ scripts/teamcity/20.test.sh
@@ -5,4 +5,9 @@ set -eu
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
-${GO} test -v -json ./...
+# Build qdb_nats_connector
+(
+    pushd ${BASE_DIR}
+    ${GO} test -v -json ./...
+    popd
+)
